77997443f31941a965049ed4451f3c4a7fbd238a
CliutonMauro <cliutonmauro@gmail.com>
2026-05-28 22:23:37 -0300

    Correção no 'ex020' - função substituida!

--- ex020.py
+++ ex020.py
@@ -5,5 +5,5 @@ aluno2 = str(input('Digite o nome do aluno: '))
 aluno3 = str(input('Digite o nome do aluno: '))
 aluno4 = str(input('Digite o nome do aluno: '))
 
-lista_random = rd.sample([aluno, aluno2, aluno3, aluno4], k=4)
+lista_random = rd.shuffle([aluno, aluno2, aluno3, aluno4])
 print(f"A ordem de apresentação será: {lista_random}")
